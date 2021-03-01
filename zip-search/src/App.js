89746abc4c400302955a956c1d28fd484be07361
AlfredoMora06 <agmmendez123@gmail.com>
2021-03-01 14:27:17 -0500

    caught up

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,23 +3,57 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div>
+      This is the City component
+    </div>
+  );
 }
 
-function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+function ZipSearchField({ onZipChange }) {
+  return (
+    <div>
+      <label>Zip Code:</label>
+      <input type="text" onChange={onZipChange} />
+    </div>
+  );
 }
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: '',
+      cities: [],
+    }
+  }
+
+  zipChanged(e) {
+    // Make GET request for the zip resource
+    // then, when you receive the result, store it in state
+    fetch('PUT_URL_HERE')
+      .then()
+      .then()
+
+    this.setState({
+      zipCode: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField onZipChange={(e) => this.zipChanged(e)} />
         <div>
+          {/*
+            Instead of hardcoding the following 3 cities,
+            Create them dynamically from this.state.cities
+          */}
+          <City />
           <City />
           <City />
         </div>
